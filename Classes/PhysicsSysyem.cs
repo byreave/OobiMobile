d13a01cc65eabb79fc4f7cce7d9eb28af5849bdc
Byreave Luo <654297651@qq.com>
2018-08-30 11:14:39 -0600

    add physics

---
+++ Classes/PhysicsSysyem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OobiMobile.Classes
+{
+    class PhysicsSystem
+    {
+        public const float GravAcc = 10.0f;
+        public float Mass { set; get; }
+
+        public Vector2 Force { set; get; }
+
+        public PhysicsSystem(float m, Vector2 f)
+        {
+            Mass = m;
+            Force = f;
+        }
+    }
+}
